63b9a5ff4a5e36a712e2308b36e4c7e960e4eb52
张云腾 <15234711471@163.com>
2026-02-18 23:37:57 +0800

    components

---
+++ components/bsp/ft6336u_driver.h
@@ -0,0 +1,12 @@
+#ifndef __FT6336U_DRIVER_H
+#define __FT6336U_DRIVER_H
+
+#include "esp_err.h"
+#include "driver/gpio.h"
+
+esp_err_t ft6336u_driver(gpio_num_t sda,gpio_num_t scl);
+
+void ft6335u_read(int16_t* x,int16_t* y,int* state);
+
+void ft6336u_int_info(bool flag);
+#endif
